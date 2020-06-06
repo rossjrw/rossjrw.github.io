8fb0b1f16f8ccedd4009db606dee0d5838cd4a8a
Ross Williams <rossjrw@gmail.com>
2020-06-06 18:52:18 +0100

    Get everything to work

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './root.scss'
+import Vue from "vue"
+
+import App from './App.vue'
+
+const vm = new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
+  render: h => h(App),
+})
