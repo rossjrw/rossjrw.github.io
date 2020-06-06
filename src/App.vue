2d9940fd590bf50f8fb635364a9d39d900ce8337
Ross Williams <rossjrw@gmail.com>
2020-06-06 17:10:00 +0100

    Setup basic page layout

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Corner></Corner>
+    <Header></Header>
+    <section class="section">
+    </section>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Corner from '@/components/Corner.vue'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: "App",
+  components: {
+    Corner, Footer, Header
+  },
+}
+</script>
