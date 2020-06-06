fa46133b6ba00d81f0c1c378985599b1176b270b
Ross Williams <rossjrw@gmail.com>
2020-06-06 18:48:27 +0100

    Add components

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="hero is-light">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1">
+          rossjrw
+        </h1>
+        <h3 class="subtitle is-3">
+          hey look it's all my shit
+        </h3>
+      </div>
+    </div>
+  </section>
+</template>
