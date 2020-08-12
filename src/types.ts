acd7e3d04d3317e03fbc89e9b7fd955fe7c8feac
Ross Williams <rossjrw@gmail.com>
2020-08-12 14:00:44 +0100

    Make abandonded a unique tag

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,30 @@
-export interface State {
+export type Tag =
+  // Project status - must have exactly 1
+  | "completed"
+  | "working"
+  | "unfinished"
+  | "abandoned"
+  // Other tags
+  | "utility"
+
+export type State = {
   projects: ProjectList
 }
 
-export interface ProjectList {
+export type ProjectList = {
   [id: number]: Project
 }
 
-export interface Project {
+export type Project =  {
   name: string
   date: string
-  tags: string[]
+  tags: Tag[]
   desc: string
   link: ProjectLink[]
 }
 
-export interface ProjectLink {
+export type ProjectLink = {
   name: string
   href: string
 }
+
