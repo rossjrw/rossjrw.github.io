a16fb654d3504db77e81063df2335b8a50e3ec91
Ross Williams <rossjrw@gmail.com>
2020-08-12 13:47:03 +0100

    Move project parser to own file

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,12 @@
 import Vue from "vue"
 import Vuex from "vuex"
-
-Vue.use(Vuex)
-
 import toml from "toml"
 
-import { State, Project, ProjectList } from '@/types'
+import { State } from '@/types'
 import projects from '!!raw-loader!./projects.toml'
+import { makeProjectList } from '@/functions/parseProjects'
 
-function makeProjectList(projects: Project[]): ProjectList {
-  return projects.reduce(
-    (acc: ProjectList, project: Project, index: number) => {
-      acc[index] = project
-      return acc
-    }, {})
-}
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
