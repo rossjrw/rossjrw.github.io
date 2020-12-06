82f7c4397a02806794a24488f7130af5474811bf
Ross Williams <rossjrw@gmail.com>
2020-12-06 01:43:26 +0000

    Use proper header syntax

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <p class="text-5xl font-semibold text-center px-8">
+  <section>
+    <h2 class="text-5xl font-semibold text-center px-8">
       {{title}}
-    </p>
-    <p class="text-xl text-center mt-1 px-8">
+    </h2>
+    <h3 class="text-xl text-center mt-1 px-8">
       {{subtitle}}
-    </p>
+    </h3>
     <Project v-for="(project, projectId) in selectedProjects"
              :key="projectId"
              :project="project"/>
-  </div>
+  </section>
 </template>
 
 <script lang="ts">
