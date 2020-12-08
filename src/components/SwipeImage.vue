371a1cd5e4151b69cd756e757b4d62d4e54f1bcd
Ross Williams <rossjrw@gmail.com>
2020-12-08 10:44:19 +0000

    Add easing

--- src/components/SwipeImage.vue
+++ src/components/SwipeImage.vue
@@ -10,7 +10,7 @@
 
 <script lang="ts">
 import Vue from "vue"
-import gsap from "gsap"
+import gsap, { Circ } from "gsap"
 
 import { getImage } from "@/functions/images"
 
@@ -22,19 +22,25 @@ export default Vue.extend({
     this.$nextTick(() => {
       const count = Object.keys(this.$refs).length
       const holdDuration = 3
-      const swipeDuration = 1
+      const swipeDuration = 2
       // In order for the animation timeline to loop and overlap with itself,
       // each element needs its own timeline
       const masterTimeline = gsap.timeline()
       // Construct a child timeline for each element
       Object.values(this.$refs).forEach((swipeImage, index) => {
+        // Initial state
+        gsap.set(
+          swipeImage,
+          { clipPath: "polygon(50% -50%, -50% 50%, -150% -50%, -50% -150%)" }
+        )
         const timeline = gsap.timeline({
           delay: (holdDuration + swipeDuration) * index - swipeDuration / 2,
           repeat: -1,
           repeatDelay: (
             (holdDuration + swipeDuration) * count -
             holdDuration - 2 * swipeDuration
-          )
+          ),
+          defaults: { duration: swipeDuration, ease: Circ }
         })
         // Set clip to top left
         timeline.set(
@@ -44,18 +50,12 @@ export default Vue.extend({
         // Move clip to centre
         timeline.to(
           swipeImage,
-          {
-            clipPath: "polygon(150% 50%, 50% 150%, -50% 50%, 50% -50%)",
-            duration: swipeDuration
-          }
+          { clipPath: "polygon(150% 50%, 50% 150%, -50% 50%, 50% -50%)" }
         )
         // Hold, then move clip to bottom left
         timeline.to(
           swipeImage,
-          {
-            clipPath: "polygon(250% 150%, 150% 250%, 50% 150%, 150% 50%)",
-            duration: swipeDuration
-          },
+          { clipPath: "polygon(250% 150%, 150% 250%, 50% 150%, 150% 50%)" },
           `+=${holdDuration}`
         )
         // Attach this element's timeline to the start of the master timeline
